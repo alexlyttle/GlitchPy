8f3d1a02105dc29cedbf1c473eb53b31af6acdff
alexlyttle <a.j.lyttle@outlook.com>
2023-02-17 14:14:05 +0000

    Import more functions into init file

--- src/glitchpy/__init__.py
+++ src/glitchpy/__init__.py
@@ -1,1 +1,3 @@
 from .mainGlitch import fit_glitch
+from .supportGlitch import fit, glitchSignal, totalGlitchSignal, smoothComponent, averageAmplitudes, compDif2
+from .plots import plot_fitSummary, plot_correlations
